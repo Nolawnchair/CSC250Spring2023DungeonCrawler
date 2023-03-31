1678234895446a98198080eea98217c3a9f07d00
Nolawnchair <nolan.elton@cuw.edu>
2023-03-30 19:23:17 -0500

    HW#8: added the ability to toggle rooms within unity

---
+++ Dungeon Crawler/Assets/scripts/DungeonSetup.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonSetup : MonoBehaviour
+{
+    public GameObject northExit, southExit, eastExit, westExit;
+    public bool northOn, southOn, eastOn, westOn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.northExit.SetActive(this.northOn);
+        this.southExit.SetActive(this.southOn);
+        this.eastExit.SetActive(this.eastOn);
+        this.westExit.SetActive(this.westOn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
